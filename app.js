ad1f890086fc586c6cac1d1fb9acf480dea1afb4
unknown <msaudkhan488@gmail.com>
2022-02-10 11:14:48 +0500

    print lo

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ function handleError (error = {}) {
     // Using duck typing to know if we explicitly threw this error
     // If not then wrapping original error into UnexpectedError
     if (!error.requestType) { error = new errors.UnexpectedError({ original_error: error }); }
-    
+    console.log('%i', 4000)
+
     const { requestType, title, message, resolution } = error;
         status = REQUEST_TYPE_STATUS_CODE[requestType],
         body = JSON.stringify({
